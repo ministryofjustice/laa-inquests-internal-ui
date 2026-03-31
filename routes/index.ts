3ad86bfd59486e28f741b76d7ac979f0a50e39eb
tommymapp <Tommy.Mapp@justice.gov.uk>
2026-03-31 17:05:39 +0100

    Fix linting error by removing unused NextFunction reference

--- routes/index.ts
+++ routes/index.ts
@@ -1,5 +1,5 @@
 import express from 'express';
-import type { Request, Response, NextFunction } from 'express';
+import type { Request, Response } from 'express';
 
 // Create a new router
 const router = express.Router();
