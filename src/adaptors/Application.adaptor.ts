c410ae9507356137e54eac54f22bfb411f03ef50
Ashley Perrett <40239096+ashperrett@users.noreply.github.com>
2026-05-21 10:47:08 +0100

    IDDS-129 add proceeding details to overview page

--- src/adaptors/Application.adaptor.ts
+++ src/adaptors/Application.adaptor.ts
@@ -1,6 +1,13 @@
 import type { Request, Response } from "express";
 import type { ViewApplicationAdaptor } from "#src/adaptors/source/inquests-api/applications/ViewApplication/ViewApplication.adaptor.js";
 import { logger } from "#src/infrastructure/express/middleware/logger/logger.js";
+import {
+  APPLICATION_TYPES,
+  CERTIFICATE_TYPES,
+  CLIENT_ROLES,
+  LEVEL_OF_SERVICE,
+  SCOPE_OF_LIMITATION,
+} from "#src/infrastructure/locales/constants.js";
 
 export class ApplicationAdaptor {
   viewApplicationAdaptor: ViewApplicationAdaptor;
@@ -21,8 +28,37 @@ export class ApplicationAdaptor {
     );
     const application =
       await this.viewApplicationAdaptor.getApplication(applicationId);
+
+    const proceeding = application.proceedings[0];
+
+    const applicationType = APPLICATION_TYPES.find(
+      (t) => t.applicationTypeId === application.applicationType,
+    )?.applicationTypeDescription;
+
+    const certificateType = CERTIFICATE_TYPES.find(
+      (t) => t.certificateTypeId === proceeding?.certificateType,
+    )?.certificateTypeDescription;
+
+    const clientRole = CLIENT_ROLES.find(
+      (t) => t.clientRoleId === proceeding?.clientInvolvementType,
+    )?.clientRoleDescription;
+
+    const levelOfService = LEVEL_OF_SERVICE.find(
+      (t) => t.levelOfServiceId === proceeding?.levelOfService,
+    )?.levelOfServiceDescription;
+
+    const scopeLimitation = SCOPE_OF_LIMITATION.find(
+      (t) => t.scopeOfLimitationId === proceeding?.scopeLimitationHeading,
+    )?.lscopeOfLimitationDescription;
+
     res.render("application/application-overview", {
       application,
+      proceeding,
+      applicationType,
+      certificateType,
+      clientRole,
+      levelOfService,
+      scopeLimitation,
     });
   }
 }
