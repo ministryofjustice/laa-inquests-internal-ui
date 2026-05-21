871c089221d565255147cd765a8b680fd81cf889
Ashley Perrett <40239096+ashperrett@users.noreply.github.com>
2026-05-21 11:11:13 +0100

    IDDS-129 map proceedings in overview for display

--- src/adaptors/Application.adaptor.ts
+++ src/adaptors/Application.adaptor.ts
@@ -1,5 +1,6 @@
 import type { Request, Response } from "express";
 import type { ViewApplicationAdaptor } from "#src/adaptors/source/inquests-api/applications/ViewApplication/ViewApplication.adaptor.js";
+import type { Application } from "#src/adaptors/models/application.types.js";
 import { logger } from "#src/infrastructure/express/middleware/logger/logger.js";
 import { formatCurrency } from "#src/utils/formatter.js";
 import {
@@ -30,39 +31,35 @@ export class ApplicationAdaptor {
     const application =
       await this.viewApplicationAdaptor.getApplication(applicationId);
 
-    const proceeding = application.proceedings[0];
+    const proceedings = mapProceedings(application.proceedings);
 
     const applicationType = APPLICATION_TYPES.find(
       (t) => t.applicationTypeId === application.applicationType,
     )?.applicationTypeDescription;
 
-    const certificateType = CERTIFICATE_TYPES.find(
-      (t) => t.certificateTypeId === proceeding.certificateType,
-    )?.certificateTypeDescription;
-
-    const clientRole = CLIENT_ROLES.find(
-      (t) => t.clientRoleId === proceeding.clientInvolvementType,
-    )?.clientRoleDescription;
-
-    const levelOfService = LEVEL_OF_SERVICE.find(
-      (t) => t.levelOfServiceId === proceeding.levelOfService,
-    )?.levelOfServiceDescription;
-
-    const scopeLimitation = SCOPE_OF_LIMITATION.find(
-      (t) => t.scopeOfLimitationId === proceeding.scopeLimitationHeading,
-    )?.lscopeOfLimitationDescription;
-
-    const costLimit = formatCurrency(proceeding.substantiveCostLimitation);
-
     res.render("application/application-overview", {
       application,
-      proceeding,
+      proceedings,
       applicationType,
-      certificateType,
-      clientRole,
-      levelOfService,
-      scopeLimitation,
-      costLimit,
     });
   }
+}
+
+function mapProceedings(proceedings: Application["proceedings"]) {
+  return proceedings.map((p) => ({
+    ...p,
+    certificateType: CERTIFICATE_TYPES.find(
+      (t) => t.certificateTypeId === p.certificateType,
+    )?.certificateTypeDescription,
+    clientInvolvementType: CLIENT_ROLES.find(
+      (t) => t.clientRoleId === p.clientInvolvementType,
+    )?.clientRoleDescription,
+    levelOfService: LEVEL_OF_SERVICE.find(
+      (t) => t.levelOfServiceId === p.levelOfService,
+    )?.levelOfServiceDescription,
+    scopeLimitationHeading: SCOPE_OF_LIMITATION.find(
+      (t) => t.scopeOfLimitationId === p.scopeLimitationHeading,
+    )?.lscopeOfLimitationDescription,
+    substantiveCostLimitation: formatCurrency(p.substantiveCostLimitation),
+  }));
 }
