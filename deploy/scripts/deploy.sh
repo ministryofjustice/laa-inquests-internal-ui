93b9bd64dd44b062202b71b73e53c64f13cf5c1e
hevdevs <90092645+hevdevs@users.noreply.github.com>
2026-07-01 13:34:57 +0100

    update deploy secrets

--- deploy/scripts/deploy.sh
+++ deploy/scripts/deploy.sh
@@ -27,6 +27,8 @@ deploy_branch() {
                 --set env.AUTH_DIRECTORY_URL="$AUTH_DIRECTORY_URL" \
                 --set env.AUTH_POST_LOGOUT_URI="$AUTH_POST_LOGOUT_URI" \
                 --set env.AUTH_REDIRECT_URI="$AUTH_REDIRECT_URI" \
+                --set env.INQUESTS_API_CLIENT_ID="$INQUESTS_API_CLIENT_ID" \
+                --set env.AWS_SECRETS_INQUESTS_API_ENTRA_CONFIG="api-entra-config-$ENVIRONMENT" \
                 --set env.AWS_SECRETS_AUTH_CLIENT_ID="auth-client-id-$ENVIRONMENT" \
                 --set env.AWS_SECRETS_AUTH_CLIENT_SECRET="auth-client-secret-$ENVIRONMENT" \
                 --set env.AWS_SECRETS_AUTH_DIRECTORY_URL="auth-directory-url-$ENVIRONMENT" \
@@ -62,6 +64,8 @@ deploy_main() {
                           --set env.AUTH_DIRECTORY_URL="$AUTH_DIRECTORY_URL" \
                           --set env.AUTH_POST_LOGOUT_URI="$AUTH_POST_LOGOUT_URI" \
                           --set env.AUTH_REDIRECT_URI="$AUTH_REDIRECT_URI" \
+                          --set env.INQUESTS_API_CLIENT_ID="$INQUESTS_API_CLIENT_ID" \
+                          --set env.AWS_SECRETS_INQUESTS_API_ENTRA_CONFIG="api-entra-config-$ENVIRONMENT" \
                           --set env.AWS_SECRETS_AUTH_CLIENT_ID="auth-client-id-$ENVIRONMENT" \
                           --set env.AWS_SECRETS_AUTH_CLIENT_SECRET="auth-client-secret-$ENVIRONMENT" \
                           --set env.AWS_SECRETS_AUTH_DIRECTORY_URL="auth-directory-url-$ENVIRONMENT" \
