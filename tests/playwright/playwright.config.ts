3c0bfcc45475afefa895efd3b292281340b1b34b
Ben Fozard <ben.fozard@madetech.com>
2026-07-02 10:32:29 +0100

    [IDDS-205] Revert "Stage mocking changes"
    This reverts commit 142da23e29d6397f78a135a682236007de4c34df.

--- tests/playwright/playwright.config.ts
+++ tests/playwright/playwright.config.ts
@@ -57,7 +57,8 @@ export default defineConfig({
       AUTH_CLIENT_SECRET: "test-client-secret",
       AUTH_REDIRECT_URI: "http://localhost:3000/auth/callback",
       AUTH_POST_LOGOUT_URI: "http://localhost:3000",
-      INQUESTS_API_URL: TEST_CONFIG.INQUESTS_API_URL,
+      INQUESTS_API_URL:
+        "https://laa-inquests-api-uat.apps.live.cloud-platform.service.justice.gov.uk",
       MOCK_OAUTH_URL: "http://localhost:4001",
     },
   },
