77d904bbebb094d230a0d8f0c6f3d1b207e8c451
Gideon Gray <gideon.gray@justice.gov.uk>
2026-07-14 12:05:12 +0100

    [IDDS-287] Add scope table and insert text to certificate view page

--- tests/playwright/e2e/applications/view-certificate.spec.ts
+++ tests/playwright/e2e/applications/view-certificate.spec.ts
@@ -75,7 +75,9 @@ test.describe.only("View certificate page", () => {
     });
     await expect(summaryHeader).toBeVisible();
 
-    const certificateSummaryTitle = page.getByText("Certificate summary");
+    const certificateSummaryTitle = page.getByText("Certificate summary", {
+      exact: true,
+    });
     await expect(certificateSummaryTitle).toBeVisible();
 
     const certificateSummaryTable = page.locator(".govuk-summary-list", {
@@ -85,7 +87,9 @@ test.describe.only("View certificate page", () => {
     await expect(
       certificateSummaryTable.getByText("Certificate type"),
     ).toBeVisible();
-    await expect(certificateSummaryTable.getByText("Status")).toBeVisible();
+    await expect(
+      certificateSummaryTable.getByText("Status", { exact: true }),
+    ).toBeVisible();
     await expect(
       certificateSummaryTable.getByText("Effective date", { exact: true }),
     ).toBeVisible();
@@ -101,6 +105,66 @@ test.describe.only("View certificate page", () => {
     ).toBeVisible();
     await expect(
       certificateSummaryTable.getByText("Certificate limitation"),
+    ).toBeVisible();
+  });
+
+  test("should display correct insert text", async ({ page }) => {
+    await page.goto(applicationCertificateUrl);
+
+    const certificateInfoInsertText = page.getByText(
+      "This is to certify that the status of the certificate is as specified in the Certificate summary. Its scope is specified in the proceedings listed and is subject to the limitations and conditions.",
+    );
+    await expect(certificateInfoInsertText).toBeVisible();
+
+    const certificateLimitationsInsertText = page.getByText(
+      "This certificate imposes both scope and financially implications on the work to be done under it. Providers should check the limitations imposed carefully and apply for an amendment where appropriate.",
+    );
+    await expect(certificateLimitationsInsertText).toBeVisible();
+
+    const scopeInsertText = page.getByText(
+      "Payment will not be made for work undertaken outside the scope specified or in excess of the cost limit.",
+    );
+    await expect(scopeInsertText).toBeVisible();
+  });
+
+  test("should have scope table", async ({ page }) => {
+    await page.goto(applicationCertificateUrl);
+
+    const scopeHeader = page.getByRole("heading", {
+      name: "Scope",
+      level: 2,
+    });
+    await expect(scopeHeader).toBeVisible();
+    // TODO: change this to check for actual proceeding in the header
+    const scopeTableHeading = page.getByText("1. placeholder", { exact: true });
+    await expect(scopeTableHeading).toBeVisible();
+
+    const scopeTable = page.locator(".govuk-summary-list", {
+      hasText: "Description",
+    });
+
+    await expect(scopeTable.getByText("Description")).toBeVisible();
+    await expect(scopeTable.getByText("Category of law")).toBeVisible();
+    await expect(
+      scopeTable.getByText("Current proceeding status"),
+    ).toBeVisible();
+    await expect(
+      scopeTable.getByText(
+        "Date work can be commenced on the above proceeding",
+      ),
+    ).toBeVisible();
+    await expect(scopeTable.getByText("Proceeding end date")).toBeVisible();
+    await expect(scopeTable.getByText("Client involvement type")).toBeVisible();
+    await expect(
+      scopeTable.getByText("Level of service", { exact: true }),
+    ).toBeVisible();
+    await expect(
+      scopeTable.getByText("Date current level of service effective"),
+    ).toBeVisible();
+    await expect(scopeTable.getByText("Limitation")).toBeVisible();
+    await expect(scopeTable.getByText("Effective date")).toBeVisible();
+    await expect(
+      scopeTable.getByText("End date", { exact: true }),
     ).toBeVisible();
   });
 });
