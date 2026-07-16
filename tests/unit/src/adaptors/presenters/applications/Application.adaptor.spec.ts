6cb347cf6694630acd3568f7051ff85793b8ed80
Tommy Mapp <Tommy.Mapp@justice.gov.uk>
2026-07-16 15:30:55 +0100

    [IDDS-287] Update model to match API

--- tests/unit/src/adaptors/presenters/applications/Application.adaptor.spec.ts
+++ tests/unit/src/adaptors/presenters/applications/Application.adaptor.spec.ts
@@ -97,7 +97,7 @@ describe("Application adaptor", () => {
     clientAddress: "1 Test Road, London",
     firmName: "Test Solicitors",
     officeAddress: "Test Office Address, London",
-    opponentDetails: ["Cabinet Office"],
+    opponentDetails: "Cabinet Office",
     guardianName: "Not applicable",
     guardianAddress: "Not applicable",
     certificateType: "SUBSTANTIVE",
--- tests/unit/src/adaptors/source/inquests-api/applications/ApplicationAPI.adaptor.spec.ts
+++ tests/unit/src/adaptors/source/inquests-api/applications/ApplicationAPI.adaptor.spec.ts
@@ -111,7 +111,7 @@ const expectedCertificate: Certificate = {
   clientAddress: "1 Test Road, London",
   firmName: "Test Solicitors",
   officeAddress: "Test Office Address, London",
-  opponentDetails: ["Cabinet Office"],
+  opponentDetails: "Cabinet Office",
   guardianName: "Not applicable",
   guardianAddress: "Not applicable",
   certificateType: "SUBSTANTIVE",
