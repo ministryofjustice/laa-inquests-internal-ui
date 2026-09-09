8e4a44677941ff5d84924a864676ecca33c68411
Patrick Sternberg <151156506+PatSternberg@users.noreply.github.com>
2026-09-09 16:14:10 +0100

    import http error code from constants file

--- tests/playwright/factories/handlers/decisionErrors.ts
+++ tests/playwright/factories/handlers/decisionErrors.ts
@@ -1,7 +1,6 @@
 import { http, HttpResponse } from "msw";
 import { TEST_CONFIG } from "#tests/playwright/playwright.config.js";
-
-const HTTP_INTERNAL_SERVER_ERROR = 500;
+import { HTTP_INTERNAL_SERVER_ERROR } from "#tests/playwright/constants/httpStatus.js";
 
 export const FAILED_GRANT_DECISION_REFERENCE = "INQ-YYY-GRANT-500";
 export const FAILED_REFUSE_DECISION_REFERENCE = "INQ-YYY-REFUSE-500";
